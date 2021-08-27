c6633f4d9313996899e6d4d2139d434c1143e6e9
daniel-mooney <daniel.mooney180@gmail.com>
2021-08-27 23:21:39 +1000

    Centred login!

--- pages/login.js
+++ pages/login.js
@@ -2,34 +2,45 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Button, Form } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Center(props) {
+    return (
+      <div style={{width: "100%"}}>
+        <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "130vh"}}>
+          {props.children}
+        </div>
+      </div>
+    );
+  }
 
 
-export default function Home() {
+export default function Login() {
   return (
     <div className="App">
-        hello
-        <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-                </Form.Text>
-            </Form.Group>
+        <Center>
+            <div style={{width: "22em"}}>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label >Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Email"/>
+                        <Form.Text className="text-muted">
+                        </Form.Text>
+                    </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
-      
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Remember me" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Login
+                    </Button>
+                </Form>
+            </div>
+        </Center>
     </div>
   )
 }
