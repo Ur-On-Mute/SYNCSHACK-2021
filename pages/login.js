75fed76825fb576dc4400315ce7055b90b90efaf
daniel-mooney <daniel.mooney180@gmail.com>
2021-08-28 01:14:42 +1000

    nothing has changed

--- pages/login.js
+++ pages/login.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function Center(props) {
     return (
       <div style={{width: "100%"}}>
-        <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "150vh"}}>
+        <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "130vh"}}>
           {props.children}
         </div>
       </div>
@@ -16,12 +16,11 @@ function Center(props) {
 
 
 
-
 export default function Login() {
   return (
     <div className="App">
         <img src="https://www.logo.wine/a/logo/Linux/Linux-Logo.wine.svg" alt="Logo"
-        style={{position: "absolute", bottom: "315px", left: "535px", height: "285px", width: "285px"}} />
+        style={{position: "absolute", bottom: "50%", left: "40%", height: "295px", width: "295px"}} />
         <Center>
             <div style={{width: "22em"}}>
                 <Form>
