ee70f3baf273f521920450c5b11e1e17f073a959
WSaffery <william.saffery@gmail.com>
2021-08-27 23:46:15 +1000

    eee

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,49 +1,55 @@
 import React, { useState, useEffect } from 'react';
 import Carousel from 'react-bootstrap/Carousel'
 import Button from 'react-bootstrap';
+// import { StaticMathField } from 'react-mathquill'
+
+class MetaEq {
+  constructor(equation, constants) {
+    this.equation = equation;
+    this.constants = constants;
+  }
+}
+// //
+// const sMF = dynamic(() => {
+//   return(<StaticMathField latex={"1+1"}> </StaticMathField>)
+// },{ ssr: false })
+
+function Equation(props){
+  function getRandomInt(min, max) {
+    return min + Math.floor(Math.random() * (max-min));
+  }
+
+  function generate(metaEq) {
+    if (metaEq == null) {return;}
+    var equation = metaEq.equation;
+    var constants = metaEq.constants;
+    for (var i = 0; i < constants.length; i++) {
+      equation = equation.replace(constants[i], getRandomInt(0, 100))
+    }
+    return equation;
+  }
+  return (<p>{generate(props.metaEq)}</p>)
+}
+
 
 function Quiz(props) {
 
+  let testMetaEq = new MetaEq("Ax^2+Bx+C",["A","B","C"])
+
   return(
-  <div style={{width: "30%", height: "25%"}}>
-    <Carousel className="">
+  <div style={{marginTop: "2em" ,width: "30em", height: "15em", backgroundColor: "grey"}}>
+  <Carousel variant="dark" interval={null}>
   <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="/assets/t1"
-      alt="First slide"
-      width="25em"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
+    <h3>Question 1</h3>
+    <Equation metaEq={testMetaEq}> </Equation>
   </Carousel.Item>
   <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="/assets/t2"
-      alt="Second slide"
-      width="25em"
-    />
-
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
+    <h3>Question 2</h3>
+    <Equation metaEq={testMetaEq}> </Equation>
   </Carousel.Item>
   <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="/assets/t3"
-      alt="Third slide"
-      width="25em"
-    />
-
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
+    <h3>Question 3</h3>
+    <Equation metaEq={testMetaEq}> </Equation>
   </Carousel.Item>
 </Carousel>
 </div>
