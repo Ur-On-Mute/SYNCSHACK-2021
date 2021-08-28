af983e4fd5f40f49e1615cb84e7d230b8253a4f3
daniel-mooney <daniel.mooney180@gmail.com>
2021-08-28 20:36:43 +1000

    Added about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,32 @@ import React from 'react'
 export default function About() {
     return (
         <div>
-            
+            <div className="about-margin">
+                <div style={{padding: "5px"}}>
+                    <div>
+                        <h2 style={{color: "#80352d"}}>About</h2>
+                    </div>
+                </div>                    
+            </div>
+            <div className="about-margin">
+                <div style={{padding: "5px"}}>
+                    <p>
+                        Sigma Forum is a dynamic mathematics learning tool and community, 
+                        targeted towards upper secondary and University students and educators. We provide
+                        high level maths question generation, incorporating interactive visual aids.
+                    </p>
+                    <p>
+                        Sigma Forum creates a fun, community orientated environment, helping to foster
+                        a students passion for mathematics as well as facilitate their ambition to learn.
+                        Our trademark forum allows for unique collaboration opportunities between students,
+                        whether that be discussing mathematics concepts that spark their interests or
+                        or asking for help with challening problems.
+
+                    </p>
+                    
+                </div>
+                
+            </div>
         </div>
     )
 }
