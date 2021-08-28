2073f9de925563ca57ce86e6674ba5d24ec7d59e
Micah Kendall <micahk.accounts@icloud.com>
2021-08-29 01:59:19 +0800

    Syntax hightlighting, codeeditor

--- src/components/postPage.jsx
+++ src/components/postPage.jsx
@@ -10,9 +10,16 @@ import {
     SigmaMathRenderer,
   } from "./sigma_math_renderer";
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import dark from "./material_dark";
+import dark from "./tomorrow.js";
 
 const PostPage = (props) => {
+    // const [comments, setComments] = useState([
+    //     {
+    //         author: 'Jordan',
+    //         body: '"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"',
+    //         datePosted: new Date()
+    //     }
+    // ]);
 
     useEffect(() => {
         getComments();
@@ -72,7 +79,7 @@ const PostPage = (props) => {
                         )) : <br/>}
                     </div>
                     <p style={{color: '#1c1c1c', fontWeight: 500, display: 'inline-block', float: 'left'}} className='card-text'>{props.postBody}</p>
-                    <button className='btn btn-outline-danger' onClick={()=>{setShowSource(!showSource)}}>Show Source Code</button>
+                    <button onClick={()=>{setShowSource(!showSource)}}>src</button>
                     {showSource && 
                         <SyntaxHighlighter language="xml" style={dark}>
                             {props.postQuestionBody}
